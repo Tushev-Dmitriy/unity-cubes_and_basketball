8ecc30fc025bfbc583e8b35f629a0fd66f5c9d51
Tushev-Dmitriy <tushev-dmitriy@mail.ru>
2024-02-13 20:45:03 +0500

    add particles

--- Unity_Cubes_and_Basketall/Assets/Scripts/SecondBlockCollision.cs
+++ Unity_Cubes_and_Basketall/Assets/Scripts/SecondBlockCollision.cs
@@ -5,25 +5,34 @@ public class SecondBlockCollision : MonoBehaviour
     private bool gameLost = false;
     private BlockSpawner blockSpawner;
     private BestScoreScript bestScoreScript;
-    private ÑoinTextScript coinTextScript;
+    private Ð¡oinTextScript coinTextScript;
+    private ParticleSystem _particleSystem;
+    public Transform target;
+    private Vector3 direction;
+    private Vector3 lastPosition;
 
     void Start()
     {
         blockSpawner = FindObjectOfType<BlockSpawner>();
         bestScoreScript = FindObjectOfType<BestScoreScript>();
-        coinTextScript = FindObjectOfType<ÑoinTextScript>();
+        coinTextScript = FindObjectOfType<Ð¡oinTextScript>();
+        _particleSystem = GetComponent<ParticleSystem>();
+
+        lastPosition = transform.position;
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Block2_0") || collision.gameObject.CompareTag("Block2_1") ||
             collision.gameObject.CompareTag("Block2_2") || collision.gameObject.CompareTag("Block2_3"))
         {
+            DestroyWithVFX();
+
             Transform canvasTransform = GameObject.Find("Canvas").transform;            
             Transform gameOverTransform = canvasTransform.Find("GameOver");
             GameObject gameOverObject = gameOverTransform.gameObject;
 
             Transform timerTransform = canvasTransform.Find("TimerParts");
-            GameObject timerObject = timerTransform.gameObject;
+            GameObject timerObject = timerTransform.gameObject; 
 
             timerObject.SetActive(false);
             gameOverObject.SetActive(true);
@@ -46,5 +55,28 @@ public class SecondBlockCollision : MonoBehaviour
     public bool IsGameLost()
     {
         return gameLost;
+    }
+
+    void Update()
+    {
+        Vector3 deltaPosition = transform.position - lastPosition;
+        if (deltaPosition != Vector3.zero)
+        {
+            lastPosition = transform.position;
+            Vector2 direction = new Vector2(deltaPosition.x, deltaPosition.z).normalized;
+            _particleSystem.transform.forward = new Vector3(direction.x, 0f, direction.y);
+        }
+    }
+
+    private void DestroyWithVFX()
+    {
+        GetComponent<Renderer>().enabled = false;
+
+        _particleSystem.simulationSpace = ParticleSystemSimulationSpace.World;
+        direction = GetComponent<Rigidbody>().velocity.normalized;
+        _particleSystem.transform.forward = direction.normalized;
+        _particleSystem.Play();
+
+        Destroy(gameObject, _particleSystem.main.duration);
     }
 }
